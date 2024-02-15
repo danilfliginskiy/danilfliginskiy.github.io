272e82ec405df3987606203b6b018a1b81cc67fb
danilfliginskiy <58595331+danilfliginskiy@users.noreply.github.com>
2024-02-15 15:32:13 +0300

    Add files via upload

--- Ravel/home.php
+++ Ravel/home.php
@@ -8,18 +8,25 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 <html>
 <head>
 	<title>Аккаунт</title>
+
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+
      <link rel="icon" href="img/favicon.ico">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;300;400;600;700;900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/main.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;300;400;600;700;900&display=swap" rel="stylesheet">
+     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
      <div class="home">
-          <h1 class="home__title">Привет, <?php echo $_SESSION['user_name']; ?></h1>
-          <div class="home__link">
-               <a href="logout.php" class="home__link-link">Выход</a>
+          <div class="container">
+
+               <h1 class="title title_center">Привет, <?php echo $_SESSION['user_name']; ?></h1>
+               <div class="home__link">
+                    <a href="logout.php" class="home__link-link">Выход</a>
+               </div>
+
           </div>
      </div>
 </body>
--- Ravel/signup.php
+++ Ravel/signup.php
@@ -1,64 +1,76 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Регистрация</title>
+     <title>Регистрация</title>
+
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+
      <link rel="icon" href="img/favicon.ico">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;300;400;600;700;900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/main.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;300;400;600;700;900&display=swap" rel="stylesheet">
+     <link rel="stylesheet" href="css/main.css">
+
 </head>
 <body>
      <div class="signup">
-          <form action="signup-check.php" method="post" class="login__form signup__form">
-               <h2 class="text login__form-title">Регистрация</h2>
-               <?php if (isset($_GET['error'])) { ?>
-                    <p class="error"><?php echo $_GET['error']; ?></p>
-               <?php } ?>
+          <div class="container">
 
-               <?php if (isset($_GET['success'])) { ?>
-                    <p class="success"><?php echo $_GET['success']; ?></p>
-               <?php } ?>
+               <form action="signup-check.php" method="post" class="login__form signup__form">
 
-               <label>Имя</label>
-               <?php if (isset($_GET['name'])) { ?>
-                    <input type="text" 
-                         name="name" 
-                         placeholder="Name"
-                         value="<?php echo $_GET['name']; ?>"><br>
-               <?php }else{ ?>
-                    <input type="text" 
-                         name="name" 
-                         placeholder="Name"><br>
-               <?php }?>
+                    <h2 class="title">Регистрация</h2>
 
-               <label>Логин</label>
-               <?php if (isset($_GET['uname'])) { ?>
-                    <input type="text" 
-                         name="uname" 
-                         placeholder="User Name"
-                         value="<?php echo $_GET['uname']; ?>"><br>
-               <?php }else{ ?>
-                    <input type="text" 
-                         name="uname" 
-                         placeholder="User Name"><br>
-               <?php }?>
+                         <?php if (isset($_GET['error'])) { ?>
+                         <p class="error"><?php echo $_GET['error']; ?></p>
+                         <?php } ?>
 
+                         <?php if (isset($_GET['success'])) { ?>
+                         <p class="success"><?php echo $_GET['success']; ?></p>
+                         <?php } ?>
 
-               <label>Пароль</label>
-               <input type="password" 
-                    name="password" 
-                    placeholder="Password"><br>
+                    <div class="login__form-wrap">
 
-               <label>Повтор пароля</label>
-               <input type="password" 
-                    name="re_password" 
-                    placeholder="Re_Password"><br>
+                         <div class="login__form-input-wrap">
 
-               <button type="submit" class="button button_login">Регистрация</button>
-               <a href="login_page.php" class="ca">Уже есть аккаунт?</a>
-          </form>
+                              <label class="login__form-label">Имя</label>
+                              <?php if (isset($_GET['name'])) { ?>
+                                   <input class="login__form-input" type="text" name="name" placeholder="Имя" value="<?php echo $_GET['name']; ?>">
+                              <?php }else{ ?>
+                                   <input class="login__form-input" type="text" name="name" placeholder="Имя">
+                              <?php }?>
+
+                         </div>
+
+                         <div class="login__form-input-wrap">
+
+                              <label class="login__form-label">Логин</label>
+                              <?php if (isset($_GET['uname'])) { ?>
+                                   <input class="login__form-input" type="text" name="uname" placeholder="Логин" value="<?php echo $_GET['uname']; ?>">
+                              <?php }else{ ?>
+                                   <input class="login__form-input" type="text" name="uname" placeholder="Логин"><br>
+                              <?php }?>
+
+                         </div>
+
+                         <div class="login__form-input-wrap">
+
+                              <label class="login__form-label">Пароль</label>
+                              <input class="login__form-input" type="password" name="password" placeholder="Пароль"><br>
+
+                              <label class="login__form-label">Повтор пароля</label>
+                              <input class="login__form-input" type="password" name="re_password" placeholder="Повторите пароль"><br>
+
+                         </div>
+
+                         <button type="submit" class="button button_login">Регистрация</button>
+                         <a href="login_page.php" class="login__form-sign">Уже есть аккаунт?</a>
+
+                    </div>
+
+               </form>
+
+          </div>
      </div>
 </body>
 </html>
