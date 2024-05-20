299264404fae97ef43ce0b2e13bf06c27b2db6fd
danilfliginskiy <58595331+danilfliginskiy@users.noreply.github.com>
2024-05-20 15:46:37 +0300

    Add files via upload

--- Hawai/js/script.js
+++ Hawai/js/script.js
@@ -3,16 +3,16 @@ const button2 = document.getElementById('button2')
 const button3 = document.getElementById('button3')
 
 button1.addEventListener('click', () => {
-	playSound('../sounds/check.mp3') // Заменить на имя вашего аудиофайла
+	playSound('./sounds/check.mp3') // Заменить на имя вашего аудиофайла
 })
 
 button2.addEventListener('click', () => {
-	playSound('../sounds/error.mp3') // Заменить на имя вашего аудиофайла
+	playSound('./sounds/error.mp3') // Заменить на имя вашего аудиофайла
 })
 
 button3.addEventListener('click', () => {
 	// Добавлен новый обработчик события
-	playSound('../sounds/new.mp3') // Добавьте имя вашего аудиофайла
+	playSound('./sounds/new.mp3') // Добавьте имя вашего аудиофайла
 })
 
 function playSound(soundURL) {
